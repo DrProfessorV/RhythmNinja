23e5e3b5853f2f49a32dd27045957786530870d4
Harrison Cote <Harrison Cote>
2017-01-05 02:56:01 -0800

    Trynna commit some big files

---
+++ Assets/Jukebox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+
+public class Jukebox : MonoBehaviour {
+    BpmManager bpm;
+    public Material pulseMaterial;
+
+    void Start()
+    {
+        bpm = GetComponent<BpmManager>();
+        bpm.Play(100, 4);
+        BpmManager.beatEvent += OnBeat;
+    }
+
+    void OnBeat(int beat)
+    {
+        pulseMaterial.DOColor(Color.white, 0.1f).OnComplete(() => { pulseMaterial.DOColor(Color.gray, 0.1f); });
+    }
+}
---
+++ Assets/Scripts/BpmManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BpmManager : MonoBehaviour {
+
+    private float startBPM = 140.0f;
+    public static float currentBPM = 140.0f;
+    private float beatsPerSecond;
+    private int beatsPerBar;
+
+    public static int beat = 0;
+    public static int bar = 0;
+
+    public delegate void OnBeat(int beatNum);
+    public delegate void OnBar(int measure);
+    public static OnBeat beatEvent;
+    public static OnBar barEvent;
+
+    AudioSource audioSource;
+    float currentSample;
+    float nextBeatSample;
+    public float sampleOffset;
+
+    private float lastBeatTime;
+
+    public void Play(float bpm, int signature)
+    {
+        audioSource = GetComponent<AudioSource>();
+        lastBeatTime = Time.time;
+        beatsPerSecond = 60.0f / currentBPM;
+        beatsPerBar = signature;
+    }
+
+    void FixedUpdate()
+    {
+        if (Time.time - lastBeatTime >= beatsPerSecond)
+        {
+            beat++;
+            lastBeatTime = Time.time;
+            if (beatEvent != null)
+            {
+                beatEvent(beat);
+            }
+            Debug.Log("Beat");
+
+            if(beat % beatsPerBar == 0)
+            {
+                bar++;
+                if (barEvent != null)
+                {
+                    barEvent(bar);
+                }
+                Debug.Log("Bar");
+            }
+        }
+    }
+}
